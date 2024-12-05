7bcafd8be83628eede33aaf859a9900cac58a6df
joaopaulopereirarezendesesi <joaopp.rezende@gmail.com>
2024-12-05 19:58:11 -0300

    update

--- Backend/cadastro.php
+++ Backend/cadastro.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $user = 'root';
 $password = '';
-$dbname = 'hangmangame';
+$dbname = 'clc dps';
 
 $conn = new mysqli($host, $user, $password, $dbname);
 if ($conn->connect_error) {
