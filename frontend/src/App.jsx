9690da3a7cd6b156c9a616deb5acc74542bfb69b
ProfMthLuiz <mthluiz99@gmail.com>
2024-12-06 14:55:52 -0300

    iniciando frontend react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <section className="game">
+        <div className="players"></div>
+
+        <div className="word"></div>
+        <div className="chat"></div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
