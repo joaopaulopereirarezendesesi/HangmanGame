c88e6a5920a85ca224308842fad529ea4e2eab64
joaopaulopereirarezendesesi <joaopp.rezende@gmail.com>
2024-12-11 21:49:26 -0300

    tome

---
+++ Backend/controllers/ChatController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Api\Websocket;
+
+use Ratchet\ConnectionInterface;
+use Ratchet\WebSocket\MessageComponentInterface;
+
+class ChatController implements MessageComponentInterface {
+}
