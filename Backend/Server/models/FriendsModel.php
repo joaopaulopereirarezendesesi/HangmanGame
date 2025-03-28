fff1dd8dbe2b1fd1cff02f119165a900306462c9
joaopaulopereirarezensesi <joaopp.rezende@gmail.com>
2025-03-28 08:53:17 -0300

    update

--- Backend/Server/models/FriendsModel.php
+++ Backend/Server/models/FriendsModel.php
@@ -9,37 +9,58 @@ class FriendsModel
 {
     private Utils $utils;
 
-    /**
-     * Constructor of the class. Instantiates the Utils object for later use.
-     */
     public function __construct()
     {
         $this->utils = new Utils();
     }
 
-    /**
-     * Retrieves the list of friends of a user based on their ID.
-     *
-     * @param int $id The ID of the user whose friends will be retrieved
-     * @return array The list of the user's friends
-     * @throws Exception In case of error when executing the query
-     */
     public function getFriendsById(string $userId): array
     {
         try {
-            $query = "
-            SELECT u.PHOTO, u.NICKNAME, u.ONLINE
-            FROM users u
-            JOIN friends f ON (u.ID_U = f.ID_A OR u.ID_U = f.ID_U)
-            WHERE f.ID_U = :id OR f.ID_A = :id;
-        ";
+            $queryFriends = "
+            SELECT 
+                u.PHOTO AS foto,
+                u.NICKNAME AS name,
+                u.ONLINE AS status,
+                r.POSITION AS rank
+            FROM ranking r
+            JOIN users u ON r.ID_U = u.ID_U
+            WHERE r.ID_U IN (
+                SELECT CASE 
+                    WHEN f.ID_U = :id THEN f.ID_A
+                    ELSE f.ID_U
+                END
+                FROM friends f
+                WHERE f.ID_U = :id1 OR f.ID_A = :id2
+            )
+            ORDER BY r.POSITION ASC;
+            ";
+
+            $queryTotalPlayers = "
+            SELECT COUNT(*) AS total_players
+            FROM ranking;
+            ";
 
             $params = [
                 ":id" => $userId,
+                ":id1" => $userId,
+                ":id2" => $userId,
             ];
-            $result = $this->utils->executeQuery($query, $params, true);
 
-            return $result ?? [];
+            // Executando as duas consultas
+            $resultFriends = $this->utils->executeQuery($queryFriends, $params, true);
+            $resultTotalPlayers = $this->utils->executeQuery($queryTotalPlayers, [], true);
+
+            // Obtendo o total de jogadores do ranking
+            $totalPlayers = $resultTotalPlayers[0]['total_players'] ?? 0;
+
+            // Estrutura final com os amigos e o total de jogadores
+            $response = [
+                "total_players" => $totalPlayers, // total de jogadores
+                "friends" => $resultFriends,      // amigos
+            ];
+
+            return $response;
         } catch (Exception $e) {
             Utils::debug_log(
                 [
